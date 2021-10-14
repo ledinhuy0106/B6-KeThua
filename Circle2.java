1f7520cc34cbcb985fbf40aa95cd2b6893e6bafe
uyle1234 <91107969+uyle1234@users.noreply.github.com>
2021-10-14 16:10:47 +0700

    Delete Circle2.java

--- Circle2.java
+++
@@ -1,9 +0,0 @@
-package Cicle;
-   public class Circle2 extends Circle {
-      private double height;
-      public double getVolume() {
-         return this.height * super.getArea();
-     }public String toString() {
-         return "Bán kính hình trụ = " + super.getRadius() + "V Trụ = " + this.getVolume();
-    }
-}
